ab0b980d99aef0ab3310c64164b9a8dbb31e2bf3
Portillo, Gastón <gaston0403@gmail.com>
2016-04-25 20:30:40 -0300

    FInalizada la tarea sobre el html contacto
    close #2
    close #10

--- Pagina WEB/js/contacto.js
+++
@@ -1,9 +0,0 @@
-"use estrict";
-
-function EnviarDatos(){ /*aca se enviarian los datos al correo de la artistica, pero en este caso borramos*/
-  document.getElementsById('nombre').innerHTML="";
-  document.getElementsByName('apellido').innerHTML="";
-  document.getElementsByName('correo').innerHTML="";
-  document.getElementsByName('comentario').innerHTML="";
-
-}
